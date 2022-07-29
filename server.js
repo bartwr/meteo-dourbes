fc98d411ce9702ffe85219c272d60cab170b7e37
bartwr <mail@bartroorda.nl>
2022-07-29 15:12:33 +0200

    Fix

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const http = require('http');
-const https = require('https');
 const csvtojson = require('csvtojson');
 
 const returnJsonFor = async (res, key) => {
@@ -35,18 +34,7 @@ const returnCsvFile = (res, url) => {
   res.end();
 }
 
-// Certificate
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/meteo-dourbes.bartroorda.nl/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/meteo-dourbes.bartroorda.nl/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/meteo-dourbes.bartroorda.nl/chain.pem', 'utf8');
-
-const credentials = {
-  key: privateKey,
-  cert: certificate,
-  ca: ca
-};
-
-const app = async (req, res) => {
+var server = http.createServer(async (req, res) => {
 
   const allowedCsvPathNames = [
     '/data/temp.csv',
@@ -69,29 +57,12 @@ const app = async (req, res) => {
     returnCsvFile(res, req.url);
   }
 
-  else if (req.url === '/.well-known/acme-challenge/XdF0Ji6qk4V8HVCrpDNgXCF3UeG0AEVbOJQzXP0toZM') {
-    res.write('XdF0Ji6qk4V8HVCrpDNgXCF3UeG0AEVbOJQzXP0toZM.MH7QTtVkzm74wa8HpAinK1M8zrrtWdUSrqDIxNSmANQ');
-    res.end();
-  }
-
   else {
     res.end('Invalid request');
   }
 
-};
-
-// Starting both http & https servers
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
-
-// server.listen(5000);
-
-// httpServer.listen(80, () => {
-//   console.log('HTTP Server running on port 80');
-// });
-
-httpsServer.listen(5000, () => {
-  console.log('HTTPS Server running on port 443');
 });
+
+server.listen(5000);
 
 console.log('Node.js web server at port 5000 is running..')
